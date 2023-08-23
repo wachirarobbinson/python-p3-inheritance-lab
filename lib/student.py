1ca034dfb2a3837aa107c04572d5ee360c1ff4be
wachirarobbinson <ngirigacharobbinson@gmail.com>
2023-08-23 12:35:48 +0300

    completed assignment

--- lib/student.py
+++ lib/student.py
@@ -3,6 +3,10 @@
 from user import User
 
 class Student(User):
+
+    def __init__(self, first_name, last_name) -> None:
+        super().__init__(first_name, last_name)
+        self.knowledge = []
     
-    def learn(self):
-        pass
+    def learn(self, lesson):
+        self.knowledge.append(lesson)
--- lib/teacher.py
+++ lib/teacher.py
@@ -6,5 +6,20 @@ import random
 
 class Teacher(User):
 
+    def __init__(self, first_name, last_name) -> None:
+        super().__init__(first_name, last_name)
+        self.knowledge = [
+            "str is a data type in Python",
+            "programming is hard, but it's worth it",
+            "JavaScript async web request",
+            "Python function call definition",
+            "object-oriented teacher instance",
+            "programming computers hacking learning terminal",
+            "pipenv install pipenv shell",
+            "pytest -x flag to fail fast",
+        ]
+
     def teach(self):
-        pass
+        random_knowledge_index = int((len(self.knowledge)-1) * random.random())
+        return self.knowledge[random_knowledge_index]
+        
